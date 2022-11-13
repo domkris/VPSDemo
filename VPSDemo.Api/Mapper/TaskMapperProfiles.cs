4fe399c05f6d1776de9acc6a8aa3729349a24d2e
Domagoj Kristov <domagojkk@gmail.com>
2022-11-13 22:31:32 +0100

    Adding AutoMapper

---
+++ VPSDemo.Api/Mapper/TaskMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using VPSDemo.Api.Contracts.Task;
+
+namespace VPSDemo.Api.Mapper
+{
+    public class TaskMapperProfiles: Profile
+    {
+        public TaskMapperProfiles()
+        {
+            // Source Domain -> Destination API
+            CreateMap<Domain.Entities.Task, TaskResponse>()
+                .ForMember(dest => dest.Identifier, option => option.MapFrom(source => source.Id))
+                .ForMember(dest => dest.CreationDate, option => option.MapFrom(source => source.CreationDate.ToString("s")));
+
+        }
+    }
+}
