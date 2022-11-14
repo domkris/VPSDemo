45c91309d0df363f5140f9bf8da3e9fab9d380a5
Domagoj Kristov <domagojkk@gmail.com>
2022-11-14 12:29:54 +0100

    added errorMessage to ErrorsController

--- VPSDemo.Api/Controllers/ErrorsController.cs
+++ VPSDemo.Api/Controllers/ErrorsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VPSDemo.Application.Common;
+
 namespace VPSDemo.Api.Controllers
 {
     [Route("/error")]
@@ -10,6 +12,7 @@ namespace VPSDemo.Api.Controllers
         public IActionResult Error()
         {
             Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+            HttpContext.Items[ConstKeys.ErrorMessage] = new string[] { $"{exception?.Message}" };
             return Problem(title: exception?.Message);
         }
     }
